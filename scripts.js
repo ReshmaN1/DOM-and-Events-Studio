9906ce1ff66e3a493c59fcce00fbde2618a3d82f
Reshma Narute <reshmanarute@Mahendras-MacBook-Air.local>
2021-07-18 20:20:06 -0500

    modified code

--- scripts.js
+++ scripts.js
@@ -1,58 +1,51 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
- let topp = 0;
-let leftt = 0;
+
+let topTracker = 0;
+let leftTracker = 0;
 
 window.addEventListener('load', function () {
-    console.log('Its loaded!');
+   console.log('Its loaded!');
+    
 
 
-    document.getElementById('up').addEventListener('click', function (){
-        //console.log("Hello");
+
+
+
+    document.getElementById('up').addEventListener('click', function () {
         let myHeight = document.getElementById('spaceShuttleHeight').innerText;
         myHeight = Number(myHeight) + 10000;
         document.getElementById('spaceShuttleHeight').innerText = myHeight;
-        let rocket = document.getElementById('rocket');
-       rocket.style.top = myHeight + "px";
-        
-        
-       
 
+        let rocket = document.getElementById('rocket');
+        topTracker = topTracker - 10;
+        rocket.setAttribute("style", "top: " + topTracker + "px: position: relative;")  
     });
 
-    document.getElementById('down').addEventListener('click', function (){
+    document.getElementById('down').addEventListener('click', function () {
         let myHeight = document.getElementById('spaceShuttleHeight').innerText;
         myHeight = Number(myHeight) - 10000;
         document.getElementById('spaceShuttleHeight').innerText = myHeight;
 
-     let rocket = document.getElementById('rocket');
-     let rocketHeight = parseInt(rocket.style.top);
-     console.log(rocketHeight);
-        rocket.style.top = rocketHeight + 10000+ "px" ;
-        
-        
-       
-
+        let rocket = document.getElementById('rocket');
+        topTracker = topTracker + 10;
+        rocket.setAttribute("style", "top: " + topTracker + "px; left: " + leftTracker + "px; position: relative;")
     });
 
     document.getElementById('left').addEventListener('click', function () {
         let rocket = document.getElementById('rocket');
-        leftt = leftt - 10;
-        alert(leftt)
-        rocket.setAttribute("style", "top: " + topp + "px;  left: " + leftt + "px; position: relative:");
-
+        leftTracker = leftTracker - 10;
+        rocket.setAttribute("style", "top: " + topTracker + "px; left: " + leftTracker + "px; position: relative;")
     });
-
+    
     document.getElementById('right').addEventListener('click', function () {
         let rocket = document.getElementById('rocket');
-        alert(leftt);
-        leftt = leftt + 10;
-        rocket.setAttribute("style", "top: " + topp + "px;  left: " + leftt + "px; position: relative:");
-
+        leftTracker = leftTracker + 10;
+        rocket.setAttribute("style", "top: " + topTracker + "px; left: " + leftTracker + "px; position: relative;")
     });
 
 
-    document.getElementById('missionAbort').addEventListener('click', function (){
+    document.getElementById('missionAbort').addEventListener('click', function () {
         let response = window.confirm("Confirm that you want to abort the mission.")
        if (response) {
         document.getElementById('flightStatus').innerText = "Mission aborted.";
@@ -81,4 +74,4 @@ window.addEventListener('load', function () {
 
 
 
-});
+}); 
